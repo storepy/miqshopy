5afea6738172e0fd8cccb1bf8dfcd75230ac5ef8
Michaël <michael.gainyo@gmail.com>
2023-01-03 13:34:15 +0100

    bump

--- shopy/sales/__init__.py
+++ shopy/sales/__init__.py
@@ -3,5 +3,5 @@ __app_name__ = 'sales'
 __author__ = "marqetintl"
 __co_author__ = "Michaël Gainyo"
 __email__ = "michaelgainyo@gmail.com"
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __copyright__ = "Copyright (C) 2021 Michaël Gainyo and contributors"
